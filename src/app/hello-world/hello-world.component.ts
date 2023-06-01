b2fb48fbda64ae433913173ca5df8fb5910c0f5d
Klea <kleaprifti21@gmail.com>
2023-06-01 11:25:58 +0200

    first commit

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: `
+    <h1>Hello World!</h1>
+  `,
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent {}
+

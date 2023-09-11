4b0b738402f868d3491bfd8ec636b5f17b5d980c
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-09-11 11:05:57 +0200

    confimation modal for add appointment

--- src/app/add-appointment-modal/add-appointment-modal.component.ts
+++ src/app/add-appointment-modal/add-appointment-modal.component.ts
@@ -2,15 +2,14 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ModalComponent } from '../confirmation-modal/confirmation-modal.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AppointmentService } from '../appointment.service'; // Assuming you have an AppointmentService
-import { UserService } from '../user.service'; // Assuming you have a UserService
+import { AppointmentService } from '../appointment.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-add-appointment-modal',
   templateUrl: './add-appointment-modal.component.html',
   styleUrls: ['./add-appointment-modal.component.css']
 })
-
 export class AddAppointmentModalComponent {
   @Output() result: EventEmitter<string> = new EventEmitter<string>();
   errorMessage: string | undefined;
@@ -18,62 +17,64 @@ export class AddAppointmentModalComponent {
   appointments: any[] = [];
   actionType: string | undefined;
   appointmentForm: FormGroup;
-  BsModalRef: any;
-  appointmentService: any;
+  modalRef!: BsModalRef;
+  confirmed!: boolean;
+  doctorId!: number | null;
+  patientId!: number | null;
 
   constructor(
     private formBuilder: FormBuilder,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private appointmentService: AppointmentService,
+    private userService: UserService 
   ) {
     this.appointmentForm = this.formBuilder.group({
       appointmentDateTime: ['', Validators.required],
     });
   }
 
-  // Rest of your code...
+  showConfirmationModal() {
+    this.modalRef = this.modalService.show(ModalComponent, {
+      initialState: {
+        actionType: 'confirmation',
+        modalTitle: 'Confirm',
+        modalMessage: 'Are you sure you want to add this appointment?',
+      },
+    });
 
+    this.modalRef.content.confirmed.subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.submitForm();
+      }
+    });
+  }
 
-//     submitForm() {
-//       const currentDate = new Date(); // Get the current date and time as a local date object
-//       const selectedDate = new Date(this.appointmentDateTime); // Get the selected date and time as a local date object
-  
-//       if (selectedDate > currentDate) {
-//         this.result.emit('success');
-//       } else {
-//         this.errorMessage = "It's not possible to add an appointment in the past";
-//         this.result.emit('error');
-//       }
-//     }
-  
-//   cancel() {
-//     this.result.emit('cancel');
-//   }
-// }
-submitForm() {
-  const currentDate = new Date(); 
-  const selectedDate = new Date(this.appointmentDateTime); 
+  submitForm() {
+    const currentDate = new Date();
+    const selectedDate = new Date(this.appointmentDateTime);
 
-  if (selectedDate <= currentDate) {
-    this.errorMessage = "It's not possible to add an appointment in the past";
-    this.result.emit('error');
-  } else {
-   this.appointmentService.addAppointment(selectedDate).subscribe(
-  () => {
-    this.showSuccessModal('Appointment added successfully');
-    this.loadAppointments();
-  },
-  (error: { message: string; }) => {
-    if (error.message === "It's not possible to add an appointment in the past") {
-      this.showErrorModal("It's not possible to add an appointment in the past");
-    } else if (error.message === 'The time slot is not available') {
-      this.showErrorModal('The time slot is not available');
+    if (selectedDate <= currentDate) {
+      this.errorMessage = "It's not possible to add an appointment in the past";
+      this.result.emit('error');
     } else {
-      console.error('Error adding appointment:', error);
+      this.appointmentService.addAppointment(selectedDate).subscribe(
+        () => {
+          this.showSuccessModal('Appointment added successfully');
+          this.loadAppointments();
+        },
+        (error: { message: string }) => {
+          if (error.message === "It's not possible to add an appointment in the past") {
+            this.showErrorModal("It's not possible to add an appointment in the past");
+          } else if (error.message === 'The time slot is not available') {
+            this.showErrorModal('The time slot is not available');
+          } else {
+            console.error('Error adding appointment:', error);
+          }
+        }
+      );
     }
   }
-);
-  }
-}
+
   showSuccessModal(message: string) {
     const successModalRef: BsModalRef = this.modalService.show(ModalComponent, {
       initialState: {
@@ -82,16 +83,14 @@ submitForm() {
         modalMessage: message,
       },
     });
-  
+
     successModalRef.content.confirmed.subscribe(() => {
-      // You can perform any actions here after the user confirms the modal
-      this.loadAppointments(); // Refresh the appointment list if needed
+      this.loadAppointments();
     });
   }
-  
+
   loadAppointments() {
-    // Assuming you have an appointment service that fetches appointments
-    this.appointmentService.getAppointments().subscribe(
+    this.appointmentService.getAppointments(this.doctorId, this.patientId).subscribe(
       (appointments: any) => {
         this.appointments = appointments;
       },
@@ -100,7 +99,7 @@ submitForm() {
       }
     );
   }
-  
+
   showErrorModal(errorMessage: string) {
     const errorModalRef: BsModalRef = this.modalService.show(ModalComponent, {
       initialState: {
@@ -108,15 +107,11 @@ submitForm() {
         modalMessage: errorMessage,
       },
     });
-  
-    errorModalRef.content.confirmed.subscribe(() => {
-    });
+
+    errorModalRef.content.confirmed.subscribe(() => {});
   }
 
   cancel() {
-    this.BsModalRef.hide();
+    this.modalRef.hide();
   }
-    get showYesButton(): boolean {
-      return this.actionType === 'confirmation';
-    }
 }

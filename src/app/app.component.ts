5fa8d8dd4dc6cd2ce309d80eb03d532de92bb232
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-10-18 11:42:08 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { environment } from '@environments/dev-environment/environment.development';'@environments/prod-environment/environment.prod';
+// import { environment } from '@environments/dev-environment/environment.development';'@environments/prod-environment/environment.prod';
+import { environment } from '@environments/prod-environment/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: '<p>API URL: {{ apiUrl }}</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'first-angular-project';
-  
+  apiUrl = environment.apiUrl;
+
 }

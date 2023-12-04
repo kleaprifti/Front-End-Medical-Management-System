2ac6f215a4582a34d49fd538a7e9dd18f7b3a2cb
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-12-04 17:44:07 +0100

    authservice

---
+++ src/app/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/login';
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const body = { username, password };
+    return this.http.post(this.apiUrl, body);
+  }
+}

524837fa72670968826d3e645193639cb540743e
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-12-14 10:41:52 +0100

    last commit

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,60 +1,3 @@
-// import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { LoginService } from '../login.service';
-
-// @Component({
-//   selector: 'app-welcome',
-//   templateUrl: './welcome.component.html',
-//   styleUrls: ['./welcome.component.css']
-// })
-// export class WelcomeComponent {
-//   loginForm!: FormGroup;
-//   username: string = 'romeisaaliu1@gmail.com';
-//   password: string = 'password';
-//   constructor(
-//     private fb: FormBuilder,
-//     private loginService: LoginService,
-//     private router: Router
-//   ) {
-//     this.createLoginForm();
-//   }
-
-//   private createLoginForm(): void {
-//     this.loginForm = this.fb.group({
-//       username: ['', [Validators.required, Validators.pattern(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)]],
-//       password: ['', Validators.required],
-//     });
-//   }
-//   private checkSessionTimeout(): void {
-//     setTimeout(() => {
-//       this.loginService.logout();
-//       this.router.navigateByUrl(''); 
-//     }, this.loginService.getSession() ? this.loginService.getSession().timeout : 0);
-//   }
-//   login(): void {
-//     if (this.loginForm.valid) {
-//       const enteredUsername = this.loginForm.value.username;
-//       const enteredPassword = this.loginForm.value.password;
-
-//       this.loginService.authenticateUser(enteredUsername, enteredPassword).subscribe(
-//         (authenticated: boolean) => {
-//           if (authenticated) {
-//             console.log('Login successful');
-//             this.router.navigate(['/appointment-list']);
-//           } else {
-//             console.error('Invalid username or password');
-//           }
-//         },
-//         (error) => {
-//           console.error('Login failed', error);
-//         }
-//       );
-//     } else {
-//       console.error('Invalid form database');
-//     }
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -69,6 +12,7 @@ export class WelcomeComponent implements OnInit {
   loginForm!: FormGroup;
   username: string = 'romeisaaliu1@gmail.com';
   password: string = 'password';
+  credentialError!: String;
 
   constructor(
     private fb: FormBuilder,
@@ -109,10 +53,11 @@ export class WelcomeComponent implements OnInit {
         },
         (error) => {
           console.error('Invalid username or password', error);
+          this.credentialError = 'Wrong username or password';
         }
       );
     } else {
-      console.error('Invalid form data');
+      console.error('Invalid form database');
     }
   }
 }
